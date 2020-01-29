10b89b3ac808d9f9760a1286fa86cfcecd6086b7
yasmeen.farag <yasmeen.farag@aurea.com>
2020-01-29 16:44:31 +0200

    add Test.java

---
+++ src/pathA/pathB/pathA/pathC/pathB/Test.java
@@ -0,0 +1,15 @@
+package pathA.pathB.pathA.pathC.pathB;
+
+public class Test {
+
+    public static void test(String input) {
+        System.out.println("Hello World!");
+
+        switch(input) {
+            case "0":
+                System.out.println('0');
+            case "1":
+                System.out.println('1');
+        }
+    }
+}
