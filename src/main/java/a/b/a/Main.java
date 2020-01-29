03d9bada79c12a7f70fb1bda0bf6660aeea32eba
yasmeen.farag <yasmeen.farag@aurea.com>
2020-01-29 13:15:42 +0200

    file with duplicate directory name in file path

---
+++ src/main/java/a/b/a/Main.java
@@ -0,0 +1,15 @@
+package main.java.a.b.a;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        switch(args[0]) {
+            case "0":
+                System.out.println('0');
+            case "1":
+                System.out.println('1');
+        }
+    }
+}
